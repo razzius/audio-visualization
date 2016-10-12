ad528dd4fc967c1b756718db9f4211acbfa70e95
Razzi Abuissa <razzi53@gmail.com>
2016-10-11 23:53:31 -0700

    Add controls and README

---
+++ README.md
@@ -0,0 +1,14 @@
+![](caribou.gif)
+
+This is an audio visualizer that uses Web Audio to read from the microphone.
+
+The user interface supports the following controls:
+
+- Switching between viewing the audio frequencies and waveform spectrum
+- Changing the length of history data stored (try lowering this if there is lag)
+- Changing the hue factor multiplied by the audio data point to specify the color
+- Changing the alpha transparency of the graph
+
+Audio-related functionality based on https://webaudiodemos.appspot.com/
+and style inspired by http://jonathanwatmough.com/2008/02/prototyping-code-in-clojure/.
+
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var context = null
 var canvasWidth = window.innerWidth
 var canvasHeight = window.innerHeight
-var dataLength = 200
+var dataLength = 100
 var analyserNode = null
 var frequencyToggle = true
+var hueMultiplier = 1.8
+var alpha = 0.2
 
 
 function range(length) {
@@ -16,10 +18,10 @@ function zeros(length) {
 }
 
 
-var BAR_WIDTH = Math.round(canvasWidth / dataLength)
+var bar_width = Math.round(canvasWidth / (dataLength - 1))
 var VERTICAL_OFFSET = 8
 
-var oldDatas = range(dataLength).map(() => zeros(64))
+var audioData = range(dataLength).map(() => zeros(64))
 
 
 function drawShape(context, points) {
@@ -44,41 +46,41 @@ function updateAnalysers() {
 
   context.clearRect(0, 0, canvasWidth, canvasHeight)
 
-  range(oldDatas.length).forEach(function(index) {
-    var i = oldDatas.length - index - 1
-    if (i == oldDatas.length - 1) return
+  range(audioData.length).forEach(function(index) {
+    var i = audioData.length - index - 1
+    if (i == audioData.length - 1) return
 
-    let newer = oldDatas[i]
-    let older = oldDatas[i + 1]
+    let newer = audioData[i]
+    let older = audioData[i + 1]
 
     range(newer.length).forEach(function(j) {
       var baseHeight = canvasHeight - 64 * VERTICAL_OFFSET
-      var oldValue = older[j]
-      var newValue = newer[j]
+      var prevValue = older[j]
+      var value = newer[j]
 
-      var xLeft = i * BAR_WIDTH + j * 2
-      var xRight = xLeft + BAR_WIDTH
+      var xLeft = i * bar_width + j * 2
+      var xRight = xLeft + bar_width
       var yBase = baseHeight + j * VERTICAL_OFFSET
 
       var bottomLeftPoint = [xLeft, yBase]
-      var topLeftPoint = [xLeft, yBase - newValue]
-      var topRightPoint = [xRight, yBase - oldValue]
+      var topLeftPoint = [xLeft, yBase - value]
+      var topRightPoint = [xRight, yBase - prevValue]
       var bottomRightPoint = [xRight, yBase]
       var shape = [bottomLeftPoint, topLeftPoint, topRightPoint, bottomRightPoint]
 
-      context.fillStyle = `hsla(${newValue * 1.8},${((newValue / 255 * .2) + .8) * 100}%,50%,.7)`
+      context.fillStyle = `hsla(${value * hueMultiplier},100%,50%,${alpha})`
       drawShape(context, shape)
     })
   })
 
-  range(oldDatas.length).forEach(function(i) {
+  range(audioData.length).forEach(function(i) {
     if (i == 0) return
 
-    var toUpdate = oldDatas.length - i
-    oldDatas[toUpdate] = oldDatas[toUpdate - 1]
+    var toUpdate = audioData.length - i
+    audioData[toUpdate] = audioData[toUpdate - 1]
   })
 
-  oldDatas[0] = freqByteData
+  audioData[0] = freqByteData
 
   window.requestAnimationFrame(updateAnalysers)
 }
@@ -101,20 +103,82 @@ function gotStream(stream) {
 
 
 function main() {
+  var toggleText = {
+    true: 'View Time Domain',
+    false: 'View Frequency',
+  }
+
   var toggle = document.createElement('button')
-  toggle.innerText = 'Toggle frequency / time domain'
+  toggle.innerText = toggleText[frequencyToggle]
   toggle.onclick = function() {
     frequencyToggle = !frequencyToggle
+    toggle.innerText = toggleText[frequencyToggle]
   }
+
+  var sliderLabel = document.createElement('label')
+  sliderLabel.innerText = 'History buffer size: '
+
+  var historySize = document.createElement('span')
+  historySize.innerText = audioData.length
+
+  var slider = document.createElement('input')
+  slider.setAttribute('type', 'range')
+  slider.setAttribute('min', 3)
+  slider.setAttribute('max', 300)
+  slider.value = audioData.length
+
+  slider.onchange = function(e) {
+    var value = e.target.value
+    if (parseInt(value) > audioData.length) {
+      audioData = audioData.concat(zeros(value - audioData.length))
+    } else {
+      audioData = audioData.slice(0, value)
+    }
+    historySize.innerText = value
+    bar_width = Math.round(canvasWidth / (audioData.length - 1))
+  }
+
+  var hueInput = document.createElement('input')
+  hueInput.value = hueMultiplier
+  hueInput.onchange = function(e) {
+    hueMultiplier = parseFloat(e.target.value)
+  }
+
+  var hueLabel = document.createElement('label')
+  hueLabel.innerText = 'Hue multiplier'
+
+  var alphaLabel = document.createElement('label')
+  alphaLabel.innerText = 'Alpha'
+
+  var alphaInput = document.createElement('input')
+  alphaInput.value = alpha
+  alphaInput.onchange = function(e) {
+    alpha = parseFloat(e.target.value)
+  }
+
   var canvas = document.createElement('canvas')
-  navigator.getUserMedia({audio: true}, gotStream, alert)
   canvas.width = canvasWidth
   canvas.height = canvasHeight
+  canvas.style['z-index'] = -1
 
   root = document.getElementById('root')
+
   root.appendChild(toggle)
+  root.appendChild(sliderLabel)
+  root.appendChild(historySize)
+  root.appendChild(slider)
+  root.appendChild(hueLabel)
+  root.appendChild(hueInput)
+  root.appendChild(alphaLabel)
+  root.appendChild(alphaInput)
+
   root.appendChild(canvas)
+
+  document.body.style['margin'] = '.3em 0 0 0'
+
   context = canvas.getContext('2d')
+
+  navigator.getUserMedia({audio: true}, gotStream, alert)
 }
 
 
