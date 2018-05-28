2010c4f0068d3e30259ff9366f2830e9cac3a54f
Razzi Abuissa <razzi53@gmail.com>
2018-05-28 09:10:50 -0700

    Lint

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
 let context = null
 let analyserNode = null
+let frequencyToggle = true
 const canvasWidth = window.innerWidth
 const canvasHeight = window.innerHeight
 const dataLength = 100
-const frequencyToggle = true
-const hueMultiplier = 1.8
-const alpha = 0.2
+let hueMultiplier = 1.8
+let alpha = 0.2
 
 
 function range(length) {
-  return Array.apply(null, Array(length)).map(function (_, i) {return i})
+  return [...Array(length)].map((_, i) => i)
 }
 
 
 function zeros(length) {
-  return Array.apply(null, Array(length)).map(() => 0)
+  return [...Array(length)].map(() => 0)
 }
 
 
-var BAR_WIDTH = Math.round(canvasWidth / (dataLength - 1))
-var VERTICAL_OFFSET = 8
+let barWidth = Math.round(canvasWidth / (dataLength - 1))
+const VERTICAL_OFFSET = 8
 
-var audioData = range(dataLength).map(() => zeros(64))
+let audioData = range(dataLength).map(() => zeros(64))
 
 
-function drawShape(context, points) {
+function drawShape(points) {
   context.beginPath()
   context.moveTo(points[0][0], points[0][1])
-  points.slice(1).forEach(function(point) {
+  points.slice(1).forEach(point => {
     context.lineTo(point[0], point[1])
   })
   context.closePath()
   context.fill()
 }
 
-// function translate(x, y) {
-//   ctxBuffer.clearRect(0,0,canvasBuffer.width,canvasBuffer.height); //clear buffer
-//   ctxBuffer.drawImage(canvasDisplay,0,0); //store display data in buffer
-//   ctxDisplay.clearRect(0,0,canvasDisplay.width,canvasDisplay.height); //clear display
-//   ctxDisplay.drawImage(canvasBuffer,x,y); //copy buffer to display
-// }
 
 function updateAnalysers() {
-  var freqByteData = new Uint8Array(64)
+  const freqByteData = new Uint8Array(64)
 
   if (frequencyToggle) {
     analyserNode.getByteFrequencyData(freqByteData)
@@ -50,11 +44,10 @@ function updateAnalysers() {
     analyserNode.getByteTimeDomainData(freqByteData)
   }
 
-  context.globalCompositeOperation = "copy";
-  context.drawImage(context.canvas,BAR_WIDTH, 0);
-  // reset back to normal for subsequent operations.
-  context.globalCompositeOperation = "source-over"
-  // context.clearRect(0, 0, canvasWidth, canvasHeight)
+  context.globalCompositeOperation = 'copy'
+  context.drawImage(context.canvas, barWidth, 0);
+  context.globalCompositeOperation = 'soft-light'
+
   const newer = audioData[0]
   const older = audioData[1]
 
@@ -63,9 +56,9 @@ function updateAnalysers() {
     const prevValue = older[j]
     const value = newer[j]
 
-    const xLeft = 50 + j * 2
+    const xLeft = j * 2
 
-    const xRight = xLeft + BAR_WIDTH
+    const xRight = xLeft + barWidth
     const yBase = baseHeight + j * VERTICAL_OFFSET
 
     const bottomLeftPoint = [xLeft, yBase]
@@ -75,13 +68,13 @@ function updateAnalysers() {
     const shape = [bottomLeftPoint, topLeftPoint, topRightPoint, bottomRightPoint]
 
     context.fillStyle = `hsla(${value * hueMultiplier},100%,50%,${alpha})`
-    drawShape(context, shape)
+    drawShape(shape)
   })
 
-  range(audioData.length).forEach(function(i) {
-    if (i == 0) return
+  range(audioData.length).forEach(i => {
+    if (i === 0) return
 
-    var toUpdate = audioData.length - i
+    const toUpdate = audioData.length - i
     audioData[toUpdate] = audioData[toUpdate - 1]
   })
 
@@ -92,10 +85,10 @@ function updateAnalysers() {
 
 
 function gotStream(stream) {
-  var audioContext = new AudioContext()
-  var inputPoint = audioContext.createGain()
+  const audioContext = new AudioContext()
+  const inputPoint = audioContext.createGain()
 
-  var audioInput = audioContext.createMediaStreamSource(stream)
+  const audioInput = audioContext.createMediaStreamSource(stream)
   audioInput.connect(inputPoint)
 
   analyserNode = audioContext.createAnalyser()
@@ -108,65 +101,65 @@ function gotStream(stream) {
 
 
 function main() {
-  var toggleText = {
+  const toggleText = {
     true: 'View Time Domain',
     false: 'View Frequency',
   }
 
-  var toggle = document.createElement('button')
+  const toggle = document.createElement('button')
   toggle.innerText = toggleText[frequencyToggle]
-  toggle.onclick = function() {
+  toggle.onclick = () => {
     frequencyToggle = !frequencyToggle
     toggle.innerText = toggleText[frequencyToggle]
   }
 
-  var sliderLabel = document.createElement('label')
+  const sliderLabel = document.createElement('label')
   sliderLabel.innerText = 'History buffer size: '
 
-  var historySize = document.createElement('span')
+  const historySize = document.createElement('span')
   historySize.innerText = audioData.length
 
-  var slider = document.createElement('input')
+  const slider = document.createElement('input')
   slider.setAttribute('type', 'range')
   slider.setAttribute('min', 3)
   slider.setAttribute('max', 300)
   slider.value = audioData.length
 
-  slider.onchange = function(e) {
-    var value = e.target.value
-    if (parseInt(value) > audioData.length) {
+  slider.onchange = e => {
+    const { value } = e.target
+    if (parseInt(value, 10) > audioData.length) {
       audioData = audioData.concat(zeros(value - audioData.length))
     } else {
       audioData = audioData.slice(0, value)
     }
     historySize.innerText = value
-    BAR_WIDTH = Math.round(canvasWidth / (audioData.length - 1))
+    barWidth = Math.round(canvasWidth / (audioData.length - 1))
   }
 
-  var hueInput = document.createElement('input')
+  const hueInput = document.createElement('input')
   hueInput.value = hueMultiplier
-  hueInput.onchange = function(e) {
+  hueInput.onchange = e => {
     hueMultiplier = parseFloat(e.target.value)
   }
 
-  var hueLabel = document.createElement('label')
+  const hueLabel = document.createElement('label')
   hueLabel.innerText = 'Hue multiplier'
 
-  var alphaLabel = document.createElement('label')
+  const alphaLabel = document.createElement('label')
   alphaLabel.innerText = 'Alpha'
 
-  var alphaInput = document.createElement('input')
+  const alphaInput = document.createElement('input')
   alphaInput.value = alpha
-  alphaInput.onchange = function(e) {
+  alphaInput.onchange = e => {
     alpha = parseFloat(e.target.value)
   }
 
-  var canvas = document.createElement('canvas')
+  const canvas = document.createElement('canvas')
   canvas.width = canvasWidth
   canvas.height = canvasHeight
   canvas.style['z-index'] = -1
 
-  root = document.getElementById('root')
+  const root = document.getElementById('root')
 
   root.appendChild(toggle)
   root.appendChild(sliderLabel)
@@ -179,10 +172,9 @@ function main() {
 
   root.appendChild(canvas)
 
-  document.body.style['margin'] = '.3em 0 0 0'
+  document.body.style.margin = '.3em 0 0 0'
 
   context = canvas.getContext('2d')
-
   navigator.getUserMedia({audio: true}, gotStream, alert)
 }
 
